dd583fb53325ffbb2fc122fc5033d40dd00bc9f5
Eli Pleaner <epleaner@calpoly.edu>
2015-07-27 14:00:01 -0700

    Set the routes contraints for the api namespace

--- market_place_api/config/routes.rb
+++ market_place_api/config/routes.rb
@@ -1,5 +1,6 @@
 MarketPlaceApi::Rails.application.routes.draw do
-  namespace :api do
-    
+  namespace :api, defaults: { format: :json},
+            constraints: { subdomain: 'api' }, path: '/' do
+  
   end
 end
